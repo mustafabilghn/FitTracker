fa5114372d2535e9e91586eeb8a969db1bc55fa8
mustafabilghn <mustafabilgehanates@hotmail.com>
2025-10-19 20:53:36 +0300

    Enable swipe-to-delete for exercises in WorkoutDetailPage
    - Added swipe-to-delete functionality in `WorkoutDetailPage.xaml`
      using `SwipeView` and bound it to a `DeleteExerciseCommand`.
    - Updated `ExerciseService` with `DeleteExerciseAsync` to handle
      exercise deletion via API.
    - Enhanced `WorkoutDetailViewModel` to support dynamic updates
      to the `Exercises` collection and handle exercise deletion.
    - Improved UI styling for exercise details in `WorkoutDetailPage`.
    - Refactored code for better readability and maintainability.

--- FitTrackr.MAUI/ViewModels/WorkoutDetailViewModel.cs
+++ FitTrackr.MAUI/ViewModels/WorkoutDetailViewModel.cs
@@ -1,4 +1,5 @@
 ﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
 using CommunityToolkit.Mvvm.Messaging;
 using FitTrackr.MAUI.Messages;
 using FitTrackr.MAUI.Models.DTO;
@@ -10,19 +11,21 @@ namespace FitTrackr.MAUI.ViewModels
     public partial class WorkoutDetailViewModel : ObservableObject
     {
         private readonly WorkoutService workoutService;
+        private readonly ExerciseService exerciseService;
 
         [ObservableProperty]
         private WorkoutDto workout;
 
         public ObservableCollection<ExerciseDto> Exercises { get; } = new();
 
-        public WorkoutDetailViewModel(WorkoutService workoutService)
+        public WorkoutDetailViewModel(WorkoutService workoutService, ExerciseService exerciseService)
         {
             this.workoutService = workoutService;
+            this.exerciseService = exerciseService;
 
             WeakReferenceMessenger.Default.Register<ExerciseAddedMessage>(this, async (r, m) =>
             {
-                if(Workout != null && Workout.Id == m.Value)
+                if (Workout != null && Workout.Id == m.Value)
                 {
                     await LoadWorkoutDetailsAsync(Workout.Id);
                 }
@@ -32,6 +35,29 @@ namespace FitTrackr.MAUI.ViewModels
         public async Task LoadWorkoutDetailsAsync(Guid workoutId)
         {
             Workout = await workoutService.GetWorkoutByIdAsync(workoutId);
+
+            Exercises.Clear();
+            if (Workout.Exercises != null)
+            {
+                foreach (var exercise in Workout.Exercises)
+                {
+                    Exercises.Add(exercise);
+                }
+            }
+        }
+
+        [RelayCommand]
+        public async Task DeleteExerciseAsync(Guid id)
+        {
+            var exercise = await exerciseService.DeleteExerciseAsync(id);
+            if (exercise != null)
+            {
+                var exerciseToRemove = Exercises.FirstOrDefault(x => x.Id == id);
+                if (exerciseToRemove != null)
+                {
+                    Exercises.Remove(exerciseToRemove);
+                }
+            }
         }
     }
 }
